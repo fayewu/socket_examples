b017eab92a8c53367f28b8dbf1f2bd96b648b40f
fayewu <fayewu37@gmail.com>
2014-02-20 22:47:56 +0800

    worker master

---
+++ deal_connect.c
@@ -0,0 +1,19 @@
+#include "utils.h"
+#include "config.h"
+#include "deal_connect.h"
+
+pid_t
+SWS_worker_init()
+{
+//	int i, listen_fd;
+//	pid_t pid;
+//
+//	listen_fd = SWS_listen(SWS_port, SWS_addr);
+//	for (i = 0; i < SWS_process_poll_size; i++) {
+//		if ((pid = fork()) > 0) {
+//			return pid;	
+//		}
+//
+//		SWS_worker_main(listen_fd);
+//	}			
+}
---
+++ deal_connect.h
@@ -0,0 +1,9 @@
+#ifndef _DEAL_CONNECT_H
+#define _DEAL_CONNECT_H
+
+#include <stdio.h>
+#include <unistd.h>
+
+extern void SWS_worker_init();
+
+#endif
